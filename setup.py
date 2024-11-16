8c081017f9b69a71ebf86e57329083bda6efcaa8
Japkeerat Singh <japkeerat21@gmail.com>
2024-11-16 13:33:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,13 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
-    install_requires=parse_requirements("./requirements.txt"),
+    install_requires=[
+        "requests==2.32.3",
+        "typer==0.13.0",
+        "beautifulsoup4==4.12.3",
+        "markdownify==0.13.1",
+        "lxml==5.3.0",
+    ],
     entry_points={
         "console_scripts": [
             "tezzcrawler=tezzcrawler.main:app",
